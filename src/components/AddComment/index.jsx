46101f6a5fc462d4e93822b7dc788594b98074bc
Vylich <ilya.sidorenko.99@gmail.com>
2023-11-19 15:42:30 +0300

    .

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -14,7 +14,7 @@ export const AddComment = ({ user, onSubmit }) => {
       <div className={styles.root}>
         <Avatar
           classes={{ root: styles.avatar }}
-          src={`${process.env.REACT_APP_API_URL}${user.avatarUrl} || '/noavatar.png'`}
+          src={user ? `${process.env.REACT_APP_API_URL}${user.avatarUrl}` : ''}
         />
         <div className={styles.form}>
           <TextField
